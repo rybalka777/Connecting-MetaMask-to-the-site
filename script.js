924e1d08358fac41cb9474949c893219a60a6973
Oleh Rybalka <safor98@gmail.com>
2023-08-30 15:08:51 +0300

    A construct that checks if an ethereum object exists in a global object

--- script.js
+++ script.js
@@ -2,5 +2,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const connectButton = document.getElementById('connectButton');
 
     async function connectToMetaMask() {
+        if (window.ethereum) {
+        }
     }
 });
